eee66fdd41175952f8157f0de14f72af4e1d84eb
Pathum Sandaruwan <iit18055@std.uwu.ac.lk>
2022-10-11 23:49:52 +0530

    set up the application and set up window

---
+++ main.py
@@ -0,0 +1,37 @@
+from tkinter import *
+import random
+
+GAME_WIDTH = 600
+GAME_HEIGHT = 600
+SPEED = 50
+SPACE_SIZE = 50
+SNAKE_PARTS = 3
+SNAKE_COLOR = "yellow"
+FOOD_COLOR ="red"
+BACKGROND_COLOR = "black"
+
+
+
+class Snake:
+    pass
+
+class Food:
+    pass
+
+def next_turn():
+    pass
+
+def change_direction(new_direction):
+    pass
+
+def check_collision():
+    pass
+
+def game_over():
+    pass
+
+window = Tk();
+window.title("Snake Game")
+window.resizable(False, False)
+
+window.mainloop()
